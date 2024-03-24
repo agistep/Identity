03099c5db344c2a7f1a05da793c61801b9674c3b
yuna <yoonha.yuna.dev@gmail.com>
2024-03-25 00:12:27 +0900

    set IdentifierProviderFactory class

---
+++ identity/src/main/java/io/agistep/identity/FixedIdentifierProviderFactory.java
@@ -0,0 +1,11 @@
+package io.agistep.identity;
+
+import io.agistep.identity.spi.IdentifierProviderFactory;
+
+public class FixedIdentifierProviderFactory implements IdentifierProviderFactory {
+
+    @Override
+    public IdentifierProvider get() {
+        return null;
+    }
+}
